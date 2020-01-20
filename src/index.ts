2881843ba3d388d024051746c2003f26fce2c811
AngrySoilder <chaudhari041@outlook.com>
2020-01-21 00:25:33 +0530

    Boilerplate for terminal

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,59 @@
 import { stdout , stderr } from 'process';
 import { Console } from 'console'
+import { Tag } from './tag';
 /**
  * Standard Handler
  * TODO: tags
  * TODO: color
  * TODO: styles
  */
-class Terminal extends Console {
+export class Terminal extends Console {
 
     // Default Const
+
+    buffer = "";
+    tags: Tag[] = [];
+
     constructor() { super(stdout, stderr); }
+
+    bold() {
+      // define bold hrere
+    }
+
+    strike() {
+      // define strike here
+    }
+
+    underline() {
+      // define underline Here
+    }
+
+    indentation() {
+      // define indentation here
+    }
+
+    direction() {
+      // define direction here
+    }
+
+    italic () {
+      // define italic here
+    }
+
+    /**
+     * Color Are Below
+     */
+
+    withcolor(color: string)
+    {
+        // Color Impl
+    }
+
+    tag(tag: string) {
+      let _tag = new Tag(tag, this);
+      this.tags.push(_tag);
+      return _tag;
+    }
+
+
 }
