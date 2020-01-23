1972dc9023235a1cefc76085aa7f466055b3a2e2
AngrySoilder <chaudhari041@outlook.com>
2020-01-24 04:33:04 +0530

    Changed Test Expectations

--- src/test/initial.test.ts
+++ src/test/initial.test.ts
@@ -8,6 +8,6 @@ test("Color Test", () =>
 {
   let color : Color = new Color(2,3,4);
   let newcolor: Color = new Color(3,4,5);
-  expect(color.toString()).toBe(';2;3;4');
-  expect(newcolor.toString()).toBe(';3;4;5');
+  expect(color.toString()).toBe('38;2;2;3;4');
+  expect(newcolor.toString()).toBe('38;2;3;4;5');
 })
